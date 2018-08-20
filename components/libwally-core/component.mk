409d2a1b49f4089c6e97125859f9d3b320f44e7e
Thomas Kerin <thomas.kerin@bitmaintech.com>
2018-08-20 18:26:24 +0200

    last state

--- components/libwally-core/component.mk
+++ components/libwally-core/component.mk
@@ -3,10 +3,13 @@ COMPONENT_OWNCLEANTARGET := true
 
 build:
 	cd $(COMPONENT_PATH) && \
-        export CFLAGS="-fno-stack-protector ${CFLAGS}" && ./tools/autogen.sh && \
-        ./configure --enable-tests=no --enable-static=no --enable-shared=no --with-asm=no --enable-lto=no --disable-swig-java --disable-js-wrappers --disable-swig-python --disable-dependency-tracking --host=xtensa-esp32-elf && \
-        $(MAKE) -o configure && \
-        $(AR) cru $(COMPONENT_BUILD_DIR)/$(COMPONENT_LIBRARY) src/secp256k1/src/libsecp256k1_la-secp256k1.o src/*.o src/ccan/ccan/crypto/sha256/libwallycore_la-sha256.o src/ccan/ccan/crypto/sha512/libwallycore_la-sha512.o src/ccan/ccan/crypto/ripemd160/libwallycore_la-ripemd160.o
+	export CFLAGS="-fno-stack-protector ${CFLAGS}" && ./tools/autogen.sh && \
+			./configure --enable-tests=no --enable-static=no --enable-shared=no \
+			--with-asm=no --enable-lto=no --disable-swig-java --disable-js-wrappers \
+			--disable-swig-python --disable-dependency-tracking --enable-static-precomputation=yes \
+			--host=xtensa-esp32-elf --target=x86_64-pc-linux-gnu --build=x86_64-pc-linux-gnu && \
+			$(MAKE) -o configure && \
+			$(AR) cru $(COMPONENT_BUILD_DIR)/$(COMPONENT_LIBRARY) src/secp256k1/src/libsecp256k1_la-secp256k1.o src/*.o src/ccan/ccan/crypto/sha256/libwallycore_la-sha256.o src/ccan/ccan/crypto/sha512/libwallycore_la-sha512.o src/ccan/ccan/crypto/ripemd160/libwallycore_la-ripemd160.o
 
 clean:
 	rm -fr $(COMPONENT_BUILD_DIR)/*.a ; \
